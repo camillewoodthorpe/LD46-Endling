899705cb6dad75aa329a88d68acc61cf27f5ec04
Camille Woodthorpe <hello@camillewoodthorpe.com>
2020-04-19 00:49:58 +0800

    Start of cutscene

---
+++ Assets/Scripts/Sequences/ToggleGameObjectSequence.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleGameObjectSequence : Sequence
+{
+    [SerializeField] private GameObject m_object;
+    [SerializeField] private bool m_state;
+    
+    
+    public override void PlaySequence()
+    {
+        m_object.SetActive(m_state);
+        FinishSequence();
+    }
+}
