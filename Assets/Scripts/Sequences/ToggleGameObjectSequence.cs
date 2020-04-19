220f0de11a27b0076946213a7513c930bf8773e3
Camille Woodthorpe <hello@camillewoodthorpe.com>
2020-04-19 09:56:13 +0800

    Start on props

--- Assets/Scripts/Sequences/ToggleGameObjectSequence.cs
+++ Assets/Scripts/Sequences/ToggleGameObjectSequence.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 
 public class ToggleGameObjectSequence : Sequence
 {
+    [SerializeField] private float m_delay = 0.0f;
+
     [SerializeField] private GameObject m_object;
     [SerializeField] private bool m_state;
     
     
     public override void PlaySequence()
     {
+        StartCoroutine(DelayedPlay());
+    }
+
+    private IEnumerator DelayedPlay()
+    {
+        yield return new WaitForSeconds(m_delay);
+        
         m_object.SetActive(m_state);
         FinishSequence();
     }
